d265d4a116d5118af0c1ca980e680472df744287
Jesus Juvinao <juvinaojesusd@gmail.com>
2024-04-07 19:13:17 -0500

    23_arrays_o_arreglos_llenado_con_%w

---
+++ 23_arrays_o_arreglos_llenado_con_%w.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# TEMA: ARRAYS O ARREGLOS  llenado con %w
+
+variable = 10
+
+# Creamos array de paises
+
+paises = ['Colombia', 'Mexico', 'USA']
+
+## llenamos con %w
+europa = %w{alemania españa francia}
+
+puts europa[0]
+
+datos = [80.5, variable, 'Hola']
+
+puts datos
