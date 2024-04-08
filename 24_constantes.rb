ad2f87368d5a7353ffa15461f9204667d89c6166
Jesus Juvinao <juvinaojesusd@gmail.com>
2024-04-07 19:26:06 -0500

    25_delimitadores_de_instruccion

---
+++ 24_constantes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# VARIABLES
+# las contantes siempre en MINUSCULAS
+hola = 'Hola'
+puts hola
+
+# CONSTANTES
+# las contantes siempre en MAYUSCULAS
+PI = 3.1416
+puts PI
+
+
+# ejemplo
+IVA = 0.16
+
+puts 5400 * IVA
---
+++ 25_delimitadores_de_instruccion.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# TEMA: Delimitadores de instrucción
+
+=begin
+  Los delimitadores de instrucción en Ruby
+  son caracteres o símbolos que se utilizan
+  para separar y terminar instrucciones o
+  expresiones en el código. Estos delimitadores
+  son esenciales para indicar al intérprete de
+  Ruby dónde comienza y termina cada instrucción,
+  lo que ayuda a que el código sea legible y comprensible.
+=end
+
+# DELIMITADOR *;*
+num1 = 10; num2 = 20; num3 = 30
+
+puts num1
+
+# DELIMITADOR *\*
+#  el \ se usan con comillas dobles para secuencias de escape
+nombre = "Lorem \
+ipsum"
+
+puts nombre
